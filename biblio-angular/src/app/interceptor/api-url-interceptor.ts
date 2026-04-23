38f011a516c75bd98eb8452c7081743b3377359d
Cyprien Aoustin <cyprien.aoustin@icloud.com>
2026-04-23 14:47:34 +0200

    Add Interceptor

---
+++ biblio-angular/src/app/interceptor/api-url-interceptor.ts
@@ -0,0 +1,10 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+
+export const apiUrlInterceptor: HttpInterceptorFn = (req, next) => {
+
+  const apiRequest = req.clone({
+    url: "http://localhost:8080/api" + req.url
+  });
+
+  return next(apiRequest);
+};
