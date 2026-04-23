4105dc38899d1e4571b402b5944d79653d70a2e7
Enora <enora.bioinfo@gmail.com>
2026-04-23 14:28:34 +0200

    Angular : Création page home, barre navigation, page collection

--- biblio-angular/src/app/app.routes.ts
+++ biblio-angular/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { CollecPage } from './page/collec-page/collec-page';
+import { HomePage } from './page/home-page/home-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomePage },
+    { path: 'collec', component: CollecPage },
+
+    { path: '', redirectTo: 'home', pathMatch: 'full' }
+];
