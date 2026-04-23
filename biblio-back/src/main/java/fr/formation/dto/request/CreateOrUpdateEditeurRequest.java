b1435a6e25dc59d442123581c26d5b9083012e43
User <cdomenger@orange.fr>
2026-04-23 11:52:35 +0200

    add DTORequestEditeur

---
+++ biblio-back/src/main/java/fr/formation/dto/request/CreateOrUpdateEditeurRequest.java
@@ -0,0 +1,28 @@
+package fr.formation.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class CreateOrUpdateEditeurRequest {
+
+	@NotBlank
+    private String nom;
+
+    @NotBlank
+    private String pays;
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPays() {
+        return pays;
+    }
+
+    public void setPays(String pays) {
+        this.pays = pays;
+    }
+}
