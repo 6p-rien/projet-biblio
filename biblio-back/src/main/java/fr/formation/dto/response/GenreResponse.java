38b0debfbfa0ce3266b90d42627f1465a5cc619b
gpichot <gael.pichot@2023.icam.fr>
2026-04-23 11:51:39 +0200

    modificattion de la classe genre

---
+++ biblio-back/src/main/java/fr/formation/dto/response/GenreResponse.java
@@ -0,0 +1,31 @@
+package fr.formation.dto.response;
+
+import fr.formation.model.Genre;
+
+public class GenreResponse {
+	private Integer id;
+	private String libelle;
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public String getLibelle() {
+		return libelle;
+	}
+
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public static GenreResponse convert (Genre genre) {
+		GenreResponse response = new GenreResponse();
+		response.setId(genre.getId());
+		response.setLibelle(genre.getLibelle());
+		return response;
+	}
+}
--- biblio-back/src/main/java/fr/formation/model/Genre.java
+++ biblio-back/src/main/java/fr/formation/model/Genre.java
@@ -1,11 +1,28 @@
 package fr.formation.model;
 
-public enum Genre {
-	Aventure("Aventure"),Fantasie("Fantasie"),Romance("Romance"),Sciencefiction("Science-fiction"),Policier("Policier"),Moderne("Moderne");
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "genre")
+public class Genre {
 	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	@Column(length = 20, nullable = false)
 	private String libelle;
 	
+	private Genre() {}
 	private Genre(String libelle) {
+		this.libelle =libelle;
+	}
+	private Genre(Integer id,String libelle) {
+		this.id=id;
 		this.libelle =libelle;
 	}
 
@@ -15,6 +32,12 @@ public enum Genre {
 
 	public void setLibelle(String libelle) {
 		this.libelle = libelle;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
 	}
 	
 }
