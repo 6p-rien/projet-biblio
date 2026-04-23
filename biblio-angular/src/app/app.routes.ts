59a820ffff3e8b33c8d50294720f6638819a21e4
Cyprien Aoustin <cyprien.aoustin@icloud.com>
2026-04-23 14:34:27 +0200

    Merge main into auteur-angular

--- biblio-angular/src/app/app.routes.ts
+++ biblio-angular/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { CollecPage } from './page/collec-page/collec-page';
 import { HomePage } from './page/home-page/home-page';
+import { AuteurPage } from './page/auteur-page/auteur-page';
 
 export const routes: Routes = [
-    { path: 'home', component: HomePage },
-    { path: 'collec', component: CollecPage },
+  { path: 'home', component: HomePage },
+  { path: 'auteur', component: AuteurPage },
+  { path: 'collec', component: CollecPage },
 
-    { path: '', redirectTo: 'home', pathMatch: 'full' }
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
 ];
