ebf026ed71714bab8f4c7ea3d83c33e8a8985564
Adam <dede13210@gmail.com>
2026-04-23 23:06:56 +0200

    front: beautify navbar and add route livre

--- biblio-angular/src/app/app.routes.ts
+++ biblio-angular/src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { CollecPage } from './page/collec-page/collec-page';
 import { HomePage } from './page/home-page/home-page';
 import { AuteurPage } from './page/auteur-page/auteur-page';
 import { EditeurPage } from './page/editeur/editeur';
+import { LivrePage } from './page/livre-page/livre-page';
 
 export const routes: Routes = [
-    { path: 'home', component: HomePage, title: "Home Page" },
-    { path: 'auteur', component: AuteurPage, title: "Liste des Auteurs" },
-    { path: 'collec', component: CollecPage, title: "Liste des collections" },
-    { path: 'editeur', component: EditeurPage, title: "Liste des éditeurs" },
+    { path: 'home', component: HomePage, title: 'Home Page' },
+    { path: 'auteur', component: AuteurPage, title: 'Liste des Auteurs' },
+    { path: 'collec', component: CollecPage, title: 'Liste des collections' },
+    { path: 'editeur', component: EditeurPage, title: 'Liste des éditeurs' },
+    { path: 'livre', component: LivrePage, title:'Liste des livres'},
 
     { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
