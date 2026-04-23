bf078a787bc08b45980ba75ebad014b784aefba0
Enora <enora.bioinfo@gmail.com>
2026-04-23 15:06:33 +0200

    Ajout title dans routes.ts + collecService

--- biblio-angular/src/app/app.routes.ts
+++ biblio-angular/src/app/app.routes.ts
@@ -3,8 +3,8 @@ import { CollecPage } from './page/collec-page/collec-page';
 import { HomePage } from './page/home-page/home-page';
 
 export const routes: Routes = [
-    { path: 'home', component: HomePage },
-    { path: 'collec', component: CollecPage },
+    { path: 'home', component: HomePage, title: "Home Page" },
+    { path: 'collec', component: CollecPage, title: "Liste des collections" },
 
     { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
